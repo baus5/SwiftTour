88ef6921843e4e2e71ce6ebfacb6406c2ae36d19
baus5 <65490597+baus5@users.noreply.github.com>
2022-12-01 16:45:55 +0300

    Update 02 Control Flow.swift

--- 02 Control Flow.swift
+++ 02 Control Flow.swift
@@ -8,6 +8,7 @@
 // Use if and switch to make conditionals, and
 // use for-in, while, and repeat-while to make loops.
 
+// if statement, for-in loop
 let individualScores = [75, 43, 103, 87, 12]
 var teamScore = 0
 
@@ -18,5 +19,29 @@ for score in individualScores {
         teamScore += 1
     }
 }
-
 print(teamScore)
+
+// Optionals, if let
+var optionalStrign: String? = "Hello"
+print(optionalStrign == nil)  // Prints "false"
+
+var optionalName: String? = "John Smith" // nil
+var greeting = "Hello!"
+
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+} else {
+    greeting = "Welcome!"  // if optionalName is nil
+}
+
+// Optional values, ?? operator, default values
+let nickname: String? = nil
+let fullName: String = "John Smith"
+let informalGreeting = "Hi \(nickname ?? fullName)"
+
+// Shorter spelling to unwrap a value
+if let nickname {
+    print("Hey, \(nickname)")
+}
+
+// Switch
