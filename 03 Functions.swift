c03954b777826d573b63c80f0200c212ac13a9ee
baus5 <65490597+baus5@users.noreply.github.com>
2022-12-03 13:19:03 +0300

    Functions and Objects

---
+++ 03 Functions.swift
@@ -0,0 +1,8 @@
+//
+//  03 Functions.swift
+//  
+//
+//  Created by baus on 3.12.2022.
+//
+
+import Foundation
---
+++ 04 Objects and Classes.swift
@@ -0,0 +1,8 @@
+//
+//  04 Objects and Classes.swift
+//  
+//
+//  Created by baus on 3.12.2022.
+//
+
+import Foundation
